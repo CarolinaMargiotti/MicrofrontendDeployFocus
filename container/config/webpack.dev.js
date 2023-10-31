4e41ed68c40c647df3e610e8c8b9d0ea924f6749
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-10-31 14:42:34 -0300

    sharing dependecies dynamically

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -2,6 +2,7 @@ const { merge } = require("webpack-merge");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const devConfig = {
 	mode: "development",
@@ -17,6 +18,8 @@ const devConfig = {
 			remotes: {
 				marketing: "marketing@http://localhost:8081/remoteEntry.js",
 			},
+			// only okay cause we are not wanting to specify extremely specific stuff in this project
+			shared: packageJson.dependencies,
 		}),
 		new HtmlWebpackPlugin({
 			template: "./public/index.html",
